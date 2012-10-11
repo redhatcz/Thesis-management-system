8751a31e4238ca173aa20aaa0c6dddb55929b946
Vaclav Dedik <vdedik@redhat.com>
2012-10-11 12:13:44 +0200

    Debug sql in development and test

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,12 +15,14 @@ environments {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            logSql = true
         }
     }
     test {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            logSql = true
         }
     }
     production {
