b358371a743bb5f18b9d729b6313dff49082f4c6
Jakub Cechacek <jcechace@redhat.com>
2012-10-15 01:46:10 +0200

    Second user and university added - for better autocomplete testing

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,6 +2,7 @@ import com.redhat.theses.University
 import com.redhat.theses.auth.User
 import com.redhat.theses.University
 import com.redhat.theses.Membership
+import com.redhat.theses.Tag
 
 class BootStrap {
 
@@ -10,6 +11,13 @@ class BootStrap {
             def u = new User(username: 'admin', fullName: 'Admin Admin', password: "admin", enabled: true).save();
             def o = new University(name: 'Masaryk University').save()
             new Membership(user: u, university: o).save()
+
+            def u2 = new User(username: 'person', fullName: 'Person Person', password: "person", enabled: true).save();
+            def o2 = new University(name: 'VUT').save()
+            new Membership(user: u2, university: o2).save()
+        }
+        if(!Tag.count()){
+            def t = new Tag(title: 'Root tag').save()
         }
     }
     def destroy = {
