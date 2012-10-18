306ef5c0ea560483c713d28acace30156d270b98
Vaclav Dedik <vdedik@redhat.com>
2012-10-18 15:14:21 +0200

    Autocompletion autoselect

--- web-app/js/a4g.js
+++ web-app/js/a4g.js
@@ -6,6 +6,30 @@
 
 UI = $.ui.autocomplete;
 
+/**
+ * This definition ensures that the written value in the autocomplete text field is selected even though the user
+ * wrote the value instead of clicking on it from the autocomplte box.
+ */
+(function( $ ) {
+    $.ui.autocomplete.prototype.options.autoSelect = true;
+    $( ".ui-autocomplete-input" ).live( "blur", function( event ) {
+        var autocomplete = $( this ).data( "autocomplete" );
+        if ( !autocomplete.options.autoSelect || autocomplete.selectedItem ) { return; }
+
+        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" );
+        autocomplete.widget().children( ".ui-menu-item" ).each(function() {
+            var item = $( this ).data( "uiAutocompleteItem" );
+            if ( matcher.test( item.label || item.value || item ) ) {
+                autocomplete.selectedItem = item;
+                return false;
+            }
+        });
+        if ( autocomplete.selectedItem ) {
+            autocomplete._trigger( "select", event, { item: autocomplete.selectedItem } );
+        }
+    });
+}( jQuery ));
+
 function autocompletion(inputFieldId, outputFieldId, url, optElements) {
     $("#" + UI.escapeRegex(inputFieldId)).autocomplete({
         source: function(request, response){
