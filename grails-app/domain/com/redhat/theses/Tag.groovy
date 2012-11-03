398a0ac99e88670f2cfa513c767b331ae3626cbd
Jakub Cechacek <jcechace@redhat.com>
2012-11-03 15:21:33 +0100

    Added allSubtags and allParents properties to Tag class

--- grails-app/domain/com/redhat/theses/Tag.groovy
+++ grails-app/domain/com/redhat/theses/Tag.groovy
@@ -7,12 +7,26 @@ class Tag {
 
     static hasMany = [subTags: Tag]
     static belongsTo = [parent: Tag]
+    static transients = ['allParents', 'allSubtags']
 
     String title
 
     static constraints = {
         title   unique: true
         parent  nullable: true
+    }
+
+    // TODO: some possible refactoring
+    List<Tag> getAllParents(){
+        def result = []
+        for (def p = parent; p != null; p = p.parent){
+            result << p
+        }
+        result
+    }
+
+    List<Tag> getAllSubTags(){
+        subTags ? subTags*.allSubTags.flatten() : []
     }
 
     String toString(){
