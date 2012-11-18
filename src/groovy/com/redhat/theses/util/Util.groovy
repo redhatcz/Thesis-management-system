f7093c8000db3fce865dbe73cb20e066d8c658c7
Vaclav Dedik <vdedik@redhat.com>
2012-11-18 14:34:46 +0100

    Damn you java!

--- src/groovy/com/redhat/theses/util/Util.groovy
+++ src/groovy/com/redhat/theses/util/Util.groovy
@@ -10,16 +10,16 @@ class Util {
     public static final Integer DEFAULT_MAX = 10
     public static final Integer MAX_LIMIT = 100
 
-    public static Integer max(maximum) {
+    static Integer max(maximum) {
         maximum = toInt(maximum)
         Math.min(maximum ?: DEFAULT_MAX, MAX_LIMIT)
     }
 
-    public static Integer lastOffset(total, maximum = null) {
+    static Integer lastOffset(total, maximum = null) {
         total - ((total % Util.max(maximum)) ?: DEFAULT_MAX)
     }
 
-    public static Integer lastOffset(total, maximum, currentOffset) {
+    static Integer lastOffset(total, maximum, currentOffset) {
         if (currentOffset == null) {
             lastOffset(total, maximum)
         } else {
@@ -27,7 +27,7 @@ class Util {
         }
     }
 
-    public static Integer toInt(object) {
+    static Integer toInt(object) {
         def result = 0
         if (object instanceof String) {
             try {
@@ -40,19 +40,19 @@ class Util {
         result
     }
 
-    public static Boolean isPaginationVisible(total, max) {
+    static Boolean isPaginationVisible(total, max) {
         toInt(total) > Util.max(max)
     }
 
-    public static Boolean isActionInUrl(HttpServletRequest request, String action) {
+    static Boolean isActionInUrl(HttpServletRequest request, String action) {
         getControllerAndAction(request)[1] == action
     }
 
-    public static Boolean isControllerInUrl(HttpServletRequest request, String controller) {
+    static Boolean isControllerInUrl(HttpServletRequest request, String controller) {
         getControllerAndAction(request)[0] == controller
     }
 
-    public static Boolean isControllerOrActionInUrl(HttpServletRequest request, String controller, String action) {
+    static Boolean isControllerOrActionInUrl(HttpServletRequest request, String controller, String action) {
         isControllerInUrl(request, controller) || isActionInUrl(request, action)
     }
 
