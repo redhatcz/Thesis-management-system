2ba58df6ba74b0e9c334f3c010e1a4b35207c7dd
Vaclav Dedik <vdedik@redhat.com>
2013-02-17 15:39:47 +0100

    Eager fetch for tags (optimization :))

--- grails-app/controllers/com/redhat/theses/TopicController.groovy
+++ grails-app/controllers/com/redhat/theses/TopicController.groovy
@@ -3,6 +3,8 @@ package com.redhat.theses
 import com.redhat.theses.auth.User
 import com.redhat.theses.util.Util
 
+import javax.persistence.FetchType
+
 class TopicController {
 
     /**
@@ -24,6 +26,8 @@ class TopicController {
     def list(Integer max) {
         params.max = Util.max(max)
         def rootTags = Tag.findAllByParentIsNull()
+        // Fetch eagerly since the tags will be needed
+        params.fetch = [tags: FetchType.EAGER]
         [topicInstanceList: Topic.list(params), topicInstanceTotal: Topic.count(), tags: rootTags]
     }
 
