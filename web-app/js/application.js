5c393a416e66306671041f628592038d88c88e59
Pavel Dedik <dedikx@gmail.com>
2013-02-28 00:12:51 +0100

    Set the minimal height of span8 to the height of span4.

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -8,8 +8,11 @@ if (typeof jQuery !== 'undefined') {
 	})(jQuery);
 }
 
-// Super simple script that sets the minimal height of the div.span8
-// as high as the height of the div.span4
+// Super simple script that sets the minimal height of the class "span8"
+// as high as the height of the class "span4"
 if ($("div.span8").height() < $("div.span4").height()) {
-    $("div.span8").height($("div.span4").height());
+
+    $("div.span8").height(
+        $("div.span4").height()
+    );
 }
