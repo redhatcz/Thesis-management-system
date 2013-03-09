fcb8f34708dfa23fe9cc458a9e4e32b832db3717
Jakub Cechacek <jcechace@redhat.com>
2013-03-09 16:12:14 +0100

    grid:link and grid:createLink now behave consitently with their g:* counterparts

--- grails-app/taglib/com/redhat/grails/mongodb/GridFileTagLib.groovy
+++ grails-app/taglib/com/redhat/grails/mongodb/GridFileTagLib.groovy
@@ -1,6 +1,6 @@
 package com.redhat.grails.mongodb
 
-// TODO: Support for files which are not bound to domain instances
+// TODO: links on files without mongoId
 class GridFileTagLib {
 
     static namespace = "grid"
@@ -12,13 +12,21 @@ class GridFileTagLib {
      *
      * @param mongoId Mongo ID of requested file.
      * @param bucket Bucket name.
+     * @params save If true download of the file will be force.
+     *
      */
     def link = { attrs, body ->
         if (!attrs.mongoId && !attrs.object) throw new IllegalArgumentException('[mongoId] attribute must be specified for the <grid:link> tag')
-        def model = [controller: 'gridFile',
-                action: 'serveFile',
-                params: attrs
-        ]
+
+        def model = attrs.findAll { !(it.key in ['mongoId', 'bucket', 'save']) }
+        model.controller = 'gridFile'
+        model.action = 'serveFile'
+        if (!model.params) {
+            model.params = [:]
+        }
+        model.params.mongoId = attrs.mongoId
+        model.params.bucket = attrs.bucket
+        model.params.save = attrs.save
 
         out << g.link(model, body)
     }
@@ -29,13 +37,21 @@ class GridFileTagLib {
      *
      * @param mongoId Mongo ID of requested file.
      * @param bucket Bucket name.
+     * @params save If true download of the file will be force.
+     *
      */
     def createLink = { attrs ->
         if (!attrs.mongoId && !attrs.object) throw new IllegalArgumentException('[mongoId] attribute must be specified for the <grid:link> tag')
-        def model = [controller: 'gridFile',
-                action: 'serveFile',
-                params: attrs
-        ]
+
+        def model = attrs.findAll { !(it.key in ['mongoId', 'bucket', 'save']) }
+        model.controller = 'gridFile'
+        model.action = 'serveFile'
+        if (!model.params) {
+            model.params = [:]
+        }
+        model.params.mongoId = attrs.mongoId
+        model.params.bucket = attrs.bucket
+        model.params.save = attrs.save
 
         out << g.createLink(model)
     }
