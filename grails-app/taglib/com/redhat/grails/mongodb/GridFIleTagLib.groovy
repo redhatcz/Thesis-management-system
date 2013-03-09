745fb8a55967903b8a4b497075b9e24a0f0a0744
Jakub Cechacek <jcechace@redhat.com>
2013-03-09 15:36:48 +0100

    embedabelinks for GridFS taglib

--- grails-app/taglib/com/redhat/grails/mongodb/GridFIleTagLib.groovy
+++ grails-app/taglib/com/redhat/grails/mongodb/GridFIleTagLib.groovy
@@ -1,5 +1,6 @@
 package com.redhat.grails.mongodb
 
+// TODO: Support for files which are not bound to domain instances
 class GridFIleTagLib {
 
     static namespace = "grid"
@@ -7,14 +8,36 @@ class GridFIleTagLib {
     def grailsLinkGenerator
     def gridFileService
 
-    def link = { attrs, body->
+    /**
+     *
+     * @param mongoId Mongo ID of requested file.
+     * @param bucket Bucket name.
+     */
+    def link = { attrs, body ->
         if (!attrs.mongoId && !attrs.object) throw new IllegalArgumentException('[mongoId] attribute must be specified for the <grid:link> tag')
         def model = [controller: 'gridFile',
-                     action: 'serveFile',
-                     params: attrs
-                     ]
+                action: 'serveFile',
+                params: attrs
+        ]
 
         out << g.link(model, body)
+    }
+
+    /**
+     *
+     * @emptyTag
+     *
+     * @param mongoId Mongo ID of requested file.
+     * @param bucket Bucket name.
+     */
+    def createLink = { attrs ->
+        if (!attrs.mongoId && !attrs.object) throw new IllegalArgumentException('[mongoId] attribute must be specified for the <grid:link> tag')
+        def model = [controller: 'gridFile',
+                action: 'serveFile',
+                params: attrs
+        ]
+
+        out << g.createLink(model)
     }
 
 }
