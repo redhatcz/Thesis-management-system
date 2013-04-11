ec84fd8af7e309912ee43da56bd225639b777d81
Vaclav Dedik <vdedik@redhat.com>
2013-04-11 19:42:18 +0200

    removed CSP from header as that causes problems in some browsers

--- grails-app/conf/com/redhat/theses/SecurityFilters.groovy
+++ grails-app/conf/com/redhat/theses/SecurityFilters.groovy
@@ -9,8 +9,6 @@ class SecurityFilters {
         all(controller:'*', action:'*') {
             after = { Map model ->
                 response.addHeader("X-Frame-Options", "DENY")
-                response.addHeader("X-Content-Security-Policy", "allow 'self';")
-                response.addHeader("X-XSS-Protection", "1; mode=block")
             }
         }
     }
