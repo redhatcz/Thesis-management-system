1e7da507257349d7eff2067f243cf2e141a2e58c
Vaclav Dedik <vdedik@redhat.com>
2013-07-21 00:13:57 +0200

    Cache avatars too

--- grails-app/controllers/com/redhat/grails/mongodb/GridFileController.groovy
+++ grails-app/controllers/com/redhat/grails/mongodb/GridFileController.groovy
@@ -6,6 +6,7 @@ class GridFileController {
     def serveFile(String mongoId, String bucket, Boolean save) {
         def file = gridFileService.getFileByUniversalMongoId(mongoId, bucket)
 
+        cache neverExpires: true
         gridFileService.serveFile(response, file, save)
     }
 }
